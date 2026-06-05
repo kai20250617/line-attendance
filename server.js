2ce4517d8b01cd7d4667254c2fd925fe62a2b95a
KAI MJ <kai384833@gmail.com>
2026-06-06 00:39:44 +0800

    fix pool ssl syntax

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const path = require("path");
 const fs = require("fs");
 const PDFDocument = require("pdfkit");
 
+const app = express();
+
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {    rejectUnauthorized: false  }
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 async function createTables() {
 
   await pool.query(`
